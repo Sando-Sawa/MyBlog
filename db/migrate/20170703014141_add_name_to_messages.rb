10f6773ed0c9c5d5dbe3d1896c31437eee053d43
Sando Sawa <soka.3.2.6.water@gmail.com>
2017-07-03 01:53:03 +0000

    new-column

---
+++ db/migrate/20170703014141_add_name_to_messages.rb
@@ -0,0 +1,5 @@
+class AddNameToMessages < ActiveRecord::Migration[5.1]
+  def change
+    add_column :messages, :temperature, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630105317) do
+ActiveRecord::Schema.define(version: 20170703014141) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170630105317) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "temperature"
   end
 
 end
